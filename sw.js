59c38ef6c007e7363855100ac1d98e5465c728f8
Jairo guzman <jairr9010@gmail.com>
2020-11-13 23:54:56 -0500

    Corrección5

--- sw.js
+++ sw.js
@@ -8,14 +8,14 @@ const APP_SHELL = [
     //'/',
     'index.html',
     'css/style.css',
-    /*'/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
-    '/js/sw-utils.js'*/
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
