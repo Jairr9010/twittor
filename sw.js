450f49530066ca8f949d5cb53c6cbd62fe62fa4e
Jairo guzman <jairr9010@gmail.com>
2020-11-13 23:50:44 -0500

    Corrección4

--- sw.js
+++ sw.js
@@ -6,9 +6,9 @@ const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
     //'/',
-    '/index.html',
-    /*'/css/style.css',
-    '/img/favicon.ico',
+    'index.html',
+    'css/style.css',
+    /*'/img/favicon.ico',
     '/img/avatars/hulk.jpg',
     '/img/avatars/ironman.jpg',
     '/img/avatars/spiderman.jpg',
@@ -28,13 +28,13 @@ const APP_SHELL_INMUTABLE = [
 
 self.addEventListener('install', e => {
 
-    /*const cacheStatic = caches.open(STATIC_CACHE)
-        .then(cache => cache.addAll(APP_SHELL));*/
+    const cacheStatic = caches.open(STATIC_CACHE)
+        .then(cache => cache.addAll(APP_SHELL));
     
     const cacheInmutable = caches.open(INMUTABLE_CACHE)
         .then(cache => cache.addAll(APP_SHELL_INMUTABLE));
     
-    e.waitUntil(Promise.all([cacheInmutable]));
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
 
 self.addEventListener('activate', e => {
